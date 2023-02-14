e12076b20611795a3a5836d04348f2de7da7928c
Taka Goto <tgoto@8thlight.com>
2023-02-14 01:00:24 -0600

    chore: fix landing page responsiveness

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,83 +3,75 @@ import * as React from 'react';
 
 export default function Header() {
   return (
-    <header className='top-0 z-30 bg-black bg-opacity-0 px-2 py-4 sm:px-4'>
-      <div className='mx-auto flex max-w-7xl items-center justify-between'>
-        <a href='#'>
-          <Image src='svg/0n1_logo.svg' alt='0n1 logo' height='45' width='45' />
-        </a>
-        <div className='flex items-center space-x-1 font-orbitron'>
-          <ul className='Namehidden space-x-2 md:inline-flex'>
-            <li className=''>
-              <a
-                href='#'
-                className='rounded px-4 py-2 text-slate-50 hover:text-[#ff494e] hover:underline'
-              >
-                HOME
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='rounded px-4 py-2 text-slate-50 hover:text-[#ff494e] hover:underline'
-              >
-                SHOP
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='rounded px-4 py-2 text-slate-50 hover:text-[#ff494e] hover:underline'
-              >
-                LORE
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='rounded px-4 py-2 text-slate-50 hover:text-[#ff494e] hover:underline'
-              >
-                BRAND
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='rounded px-4 py-2 text-slate-50 hover:text-[#ff494e] hover:underline'
-              >
-                GALLERY
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='rounded px-4 py-2 text-slate-50 hover:text-[#ff494e] hover:underline'
-              >
-                COLLECTION
-              </a>
-            </li>
-          </ul>
-          <div className='inline-flex md:hidden'>
-            <button className='flex-none px-2 '>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-6 w-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M4 8h16M4 16h16'
-                />
-              </svg>
-              <span className='sr-only'>Open Menu</span>
-            </button>
+    <div className='z-100 absolute top-0 mx-auto w-full'>
+      <nav className='mb-10 border-gray-200 px-2'>
+        <div className='container mx-auto flex flex-wrap items-center justify-between'>
+          <a href='#' className='flex pt-2'>
+            <Image
+              src='svg/0n1_logo.svg'
+              alt='0n1 logo'
+              height='45'
+              width='45'
+            />
+          </a>
+          <div
+            className='hidden w-full items-center justify-between font-orbitron md:order-1 md:flex md:w-auto'
+            id='mobile-menu-3'
+          >
+            <ul className='mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium'>
+              <li>
+                <a
+                  href='#'
+                  className='rounded px-4 py-2 text-slate-50 hover:text-[#ff494e] hover:underline'
+                  aria-current='page'
+                >
+                  HOME
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  className='rounded py-2 text-slate-50 hover:text-[#ff494e] hover:underline'
+                >
+                  SHOP
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  className='rounded py-2 text-slate-50 hover:text-[#ff494e] hover:underline'
+                >
+                  LORE
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  className='rounded py-2 text-slate-50 hover:text-[#ff494e] hover:underline'
+                >
+                  BRAND
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  className='rounded py-2 text-slate-50 hover:text-[#ff494e] hover:underline'
+                >
+                  GALLERY
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  className='rounded py-2 text-slate-50 hover:text-[#ff494e] hover:underline'
+                >
+                  COLLECTION
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
-    </header>
+      </nav>
+    </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import * as React from 'react';
 import { useEffect } from 'react';
 import { useGlitch } from 'react-powerglitch';
@@ -37,21 +38,49 @@ export default function HomePage() {
           <div>
             <div
               style={{
-                backgroundImage: `url(/images/landing_art.png)`,
                 backgroundRepeat: 'no-repeat',
               }}
               ref={glitch.ref}
-              className='min-h-screen w-fit bg-[#0c0d0d] bg-cover bg-center opacity-100 md:w-full lg:w-full xl:w-full 2xl:w-full'
-              onAnimationEnd={() => setIsAnimationDone(true)}
+              className='z-1 absolute min-h-screen w-fit bg-[#0c0d0d] bg-[url("/images/landing_art.png")] bg-cover bg-center opacity-100 md:w-full lg:block'
             >
-              <Header />
               <div className='grid min-h-screen w-full place-items-center'>
-                <div className='text-center font-orbitron text-9xl font-bold text-white'>
+                <div className='invisible text-center font-orbitron text-9xl font-bold text-white sm:invisible md:visible lg:visible xl:visible 2xl:visible'>
                   0N1Force
                 </div>
               </div>
             </div>
+            {/* <img
+              //ref={glitch.ref}
+              className='z-1 bg-opacity-1 overlay-item absolute bottom-0 h-full w-full transform object-cover object-center lg:hidden'
+              src='images/mobile_landing_art.png'
+              alt=''
+            /> */}
+            <Header />
           </div>
+          // <div>
+          //   <div
+          //     style={{
+          //       backgroundRepeat: 'no-repeat',
+          //     }}
+          //     // ref={glitch.ref}
+          //     className='z-100 invisible min-h-screen w-fit bg-[#0c0d0d] bg-[url("/images/landing_art.png")] bg-cover bg-center opacity-100 md:visible md:w-full'
+          //     onAnimationEnd={() => setIsAnimationDone(true)}
+          //   >
+          //     <Header />
+          //     <img
+          //       src='/images/mobile_landing_art.png'
+          //       width='375'
+          //       height='2788'
+          //       className='visible -z-10 lg:hidden'
+          //       alt='mobile_landing_art'
+          //     />
+          //     <div className='grid min-h-screen w-full place-items-center'>
+          //       <div className='invisible text-center font-orbitron text-9xl font-bold text-white sm:invisible md:visible lg:visible xl:visible 2xl:visible'>
+          //         0N1Force
+          //       </div>
+          //     </div>
+          //   </div>
+          // </div>
         )}
       </main>
     </Layout>
